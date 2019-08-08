3175a0f46a3d7e22938772d3fc01c1d469f0ea5c
IrvingHuang <S409896@sinyi.com.tw>
2019-08-08 16:03:22 +0800

    Initial Commit

---
+++ CICDTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CICDTest
+//
+//  Created by IrvingHuang on 2019/8/8.
+//  Copyright © 2019 sinyi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
